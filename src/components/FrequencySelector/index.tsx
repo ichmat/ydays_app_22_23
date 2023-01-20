301771836999ff9fd6b9b921555fc4fad6b541eb
Mattéo LOPES <matteo.lopes@ynov.com>
2023-01-20 08:46:52 +0100

    forget to save

---
+++ src/components/FrequencySelector/index.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { View, StyleSheet, Text, ColorValue } from "react-native";
+
+const FrequencySelector = () => {
+    
+}
+
+export default FrequencySelector
